f70eb610717e1a74fad1982ff0bc4faae620a113
jhfatehi <jhfatehi@users.noreply.github.com>
2018-06-24 00:04:09 -0700

    Add files via upload

---
+++ testdb_schema.sql
@@ -0,0 +1,33 @@
+DROP SCHEMA IF EXISTS testdb;
+CREATE SCHEMA testdb;
+USE testdb;
+
+CREATE TABLE test1 (
+	val1 int, 
+	val2 int, 
+	val3 int, 
+	val4 varchar(20), 
+	val5 float, 
+	val6 float, 
+	val7 float);
+
+CREATE TABLE test2 (
+	val1 int,
+	val2 int,
+	val3 int,
+	val4 varchar(20),
+	val5 float,
+	val6 float,
+	val7 float,
+	val8 float,
+	val9 time,
+	val10 varchar(20),
+	val11 float,
+	val12 time,
+	val13 float,
+	val14 float,
+	val15 float,
+	val16 time,
+	val17 int,
+	val18 time,
+	val19 float);
